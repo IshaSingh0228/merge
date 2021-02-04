cb2951a48eadc87f85fda8c661fe03a005539e01
IshaSingh0228 <ishasingh0228@gmail.com>
2021-02-04 17:00:01 +0530

    created 2 files test1 and test2

---
+++ merge_conflict/src/test2.java
@@ -0,0 +1,13 @@
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+System.out.println("adding value");
+System.out.println(a+b);
+
+	}
+
+}
