3decbb8e5270eb8f7ee112866cc625c55b467bce
IshaSingh0228 <ishasingh0228@gmail.com>
2021-02-08 14:05:29 +0530

    modified test1

--- merge_conflict/src/test1.java
+++ merge_conflict/src/test1.java
@@ -7,7 +7,7 @@ public class test1 {
 		int b=20;
 		System.out.println("Multiplying value");
 		System.out.println(a*b);
-		int arr[]= {1,2,3};
+		int arr[]= {1,2,3,4,5};
 		for(int i:arr) {
 			System.out.println(i);
 		}
