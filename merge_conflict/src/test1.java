9ce917748560d435dba8d72bd202e807e76400f7
IshaSingh0228 <78353412+IshaSingh0228@users.noreply.github.com>
2021-04-28 11:29:12 +0530

    changed the value of variable

--- merge_conflict/src/test1.java
+++ merge_conflict/src/test1.java
@@ -3,7 +3,7 @@ public class test1 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a=10;
+		int a=20;
 		int b=20;
 		System.out.println("Multiplying value");
 		System.out.println(a+b);
